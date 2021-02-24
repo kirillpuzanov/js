2c296caf8aa0e8cc00a76f06bf2e85b1d2f44dc4
kirillpuzanov <kirillpuzanov@mail.ru>
2021-02-24 20:31:11 +0300

    full_copy

--- js_test_scripts/3_string.js
+++ js_test_scripts/3_string.js
@@ -1,12 +1,12 @@
-const name = 'Владилен'
+const name = 'Кирилл'
 console.log(name.length)  //8  последний индекс + 1
 console.log(name.toUpperCase())  //  приводит  к верхнему регистру
 console.log(name.toLowerCase())   //  приводит  к нижнему регистру
 console.log(name.charAt(2)) // а  возвращает букву  по такому индексу
 console.log(name.indexOf('!'))  //false есть ли в строке такой символ
-console.log(name.toLowerCase().startsWith('влад'))  // true начинается ли с переданной строки
-console.log(name.startsWith('Влад'))  // true
-console.log(name.endsWith('ен!'))  // false заканчивается ли такими символами
+console.log(name.toLowerCase().startsWith('кир'))  // true начинается ли с переданной строки
+console.log(name.startsWith('кир'))  // true
+console.log(name.endsWith('лл!'))  // false заканчивается ли такими символами
 console.log(name.repeat(3))  //возвращает строку повторенную  3 раза без пробелов
 const string = '     password      '  
 console.log(string.trim())  // обрезает все пробелы
@@ -20,7 +20,7 @@ function logPerson(s, name, age) {
     return `${s[0]}${name}${s[1]}${age}${s[2]}`
   }
   
-  const personName = 'Владилен'
+  const personName = 'Кирилл'
   const personName2 = 'Максим'
   const personAge = 26
   const personAge2 = -10
